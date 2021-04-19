7506556761c152731f9ddf5db0651decaf4ba8e1
jmallarijava@gmail.com <jmallarijava@gmail.com>
2021-04-19 00:38:43 -0400

    added the files

---
+++ app/src/main/java/com/example/surfapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.surfapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.example.surfapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+}
